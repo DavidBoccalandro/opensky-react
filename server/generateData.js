085af680bf449d262bc080049cf439d1f5146ca4
DavidBoccalandro <davidboccalandro@hotmail.com>
2023-05-12 02:13:16 -0300

    :clown_face: update fake database paths

--- server/generateData.js
+++ server/generateData.js
@@ -1,20 +1,33 @@
-const { writeFile } = require('fs');
-const { resolve } = require('path');
+const { writeFileSync, readFileSync } = require('fs');
+const { join } = require('path');
 const bcrypt = require('bcrypt');
 
 const initialData = [
-  { id: 1, username: 'username1', password: 'password1' },
-  { id: 2, username: 'username2', password: 'password2' },
-  { id: 3, username: 'username3', password: 'password3' },
+  { id: 1, username: 'username1', password: 'password1', hashedPassword: '' },
+  { id: 2, username: 'username2', password: 'password2', hashedPassword: '' },
+  { id: 3, username: 'username3', password: 'password3', hashedPassword: '' },
 ];
 
-export const generateDb = () => {
+const generateDb = () => {
   const hashedData = initialData.map((item) => {
     return {
       ...item,
       hashedPassword: bcrypt.hashSync(item.password, 10),
     };
   });
+  saveDb(hashedData);
+};
 
-  writeFile(resolve(__dirname, '/db.json'), JSON.parse(hashedData));
+const saveDb = (data) => {
+  writeFileSync(join(__dirname, '/db.json'), JSON.stringify(data));
+};
+
+const loadDb = () => {
+  return JSON.parse(readFileSync(join(__dirname, '/db.json'), 'utf-8'));
+};
+
+module.exports = {
+  generateDb,
+  saveDb,
+  loadDb,
 };
