9a60795eb09f8027cc5393051b1823c4bd086e6c
DavidBoccalandro <davidboccalandro@hotmail.com>
2023-05-14 19:54:16 -0300

    :sparkles: update dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import { FlightService } from 'services/flights.services';
 import { apiSuccess } from 'utils/api.guards';
 import { flatAirportNames } from 'utils/flights.utils';
 import { useFlights } from 'hooks/useFlights';
+import { Spin } from 'antd';
 
 export const Dashboard = () => {
   const [airports, setAirports] = React.useState<string[]>([]);
@@ -25,16 +26,17 @@ export const Dashboard = () => {
     flights.loadFlightsByAirport(airport);
   }
 
-  console.log('flights', flights.flights);
-  console.log(activeAirport)
+  const data = flights.getFlightsByAirport(activeAirport);
 
   return (
-    <div>
+    <Spin spinning={flights.loading}>
       <select name="airport" id="airport" onChange={handleAirportChange}>
         {
           airports.map((airport) => <option value={airport} key={airport}>{airport.toUpperCase()}</option>)
         }
       </select>
-    </div>
+      <li key={data?.arrivals}>{data?.arrivals?.length}</li>
+      <li key={data?.arrivals}>{data?.arrivals?.length}</li>
+    </Spin>
   )
 }
